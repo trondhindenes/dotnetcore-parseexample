b9cef315b53022fffa7f6ef86a6c72f589b80069
Trond Hindenes <trond.hindenes@rikstv.no>
2017-07-09 18:19:45 +0200

    moar readme

--- Program.cs
+++ Program.cs
@@ -13,7 +13,13 @@ namespace parseexample
                 ""name"": ""trond"",
                 ""lastname"": ""hindenes""
             }";
+
+            // essentially: Take the myString variable, and convert it to the type "Person"
+            // which is defined in the "Person.cs" file.
             var myPerson = Newtonsoft.Json.JsonConvert.DeserializeObject<Person>(myString);
+
+            // since we know that "myPerson" is an object of the "Person" class, 
+            // we know that it has a "name" attribute that can be accessed directly.
             Console.WriteLine(myPerson.name);
         }
     }
